7d25c958546ee30c94ab24173dd1852a5c50106d
Urszula Suplicka <usuplicka@gmail.com>
2020-12-01 10:23:20 +0100

    Add papier i nozyce to js script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
